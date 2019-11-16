3581f4b4c72a5994c426988ecc3a47d30350090d
abhinav <bhattabhinav@github.com>
2019-11-16 09:28:42 -0500

    Updates

---
+++ precache-manifest.0940529c36524072ea618f2c35a1110b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbc224a6faa14191b9811dad093bf04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ea9a5cf4d1137e12cc8",
+    "url": "/static/css/main.e0b516ce.chunk.css"
+  },
+  {
+    "revision": "bcc0fdf5bf90bb844eee",
+    "url": "/static/js/2.2d7540f1.chunk.js"
+  },
+  {
+    "revision": "5ea9a5cf4d1137e12cc8",
+    "url": "/static/js/main.bb1389ba.chunk.js"
+  },
+  {
+    "revision": "84414123030a07608ae9",
+    "url": "/static/js/runtime-main.c91803e0.js"
+  }
+]);
